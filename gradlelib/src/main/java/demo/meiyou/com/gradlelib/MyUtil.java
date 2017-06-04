5aaf031c92f967f77ff2edbb54e64cf7643533c1
MeiyouSunny <499655607@qq.com>
2017-06-04 16:10:46 +0800

    Nexus maven

---
+++ gradlelib/src/main/java/demo/meiyou/com/gradlelib/MyUtil.java
@@ -0,0 +1,11 @@
+package demo.meiyou.com.gradlelib;
+
+/**
+ * Created by Administrator on 2017/5/21 0021.
+ */
+
+public class MyUtil {
+
+    public static final String BASE_URL = "http;//localhost:8081";
+
+}
